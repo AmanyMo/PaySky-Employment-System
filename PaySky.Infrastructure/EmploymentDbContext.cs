ba85d81c3a33bd2f874e624b51457164a337e1f3
Amany Abdel-Aziz <AmaAbdelAziz@orange.eg>
2025-02-15 16:30:51 +0200

    -Modify Connection String -ADD Migration MigV001 -Insert 2 Role into DB thru mig

--- PaySky.Infrastructure/EmploymentDbContext.cs
+++ PaySky.Infrastructure/EmploymentDbContext.cs
@@ -8,13 +8,33 @@ namespace PaySky.Infrastructure
         public DbSet<User> Users { get; set; }
         public DbSet<Vacancy> Vacancies { get; set; }
         public DbSet<Role> Roles { get; set; }
-        public DbSet<ApplicantVacancy> ApplicantVacancy { get; set; }
+        public DbSet<ApplicantVacancy> Applicant_Vacancy { get; set; }
 
 
         public EmploymentDbContext(DbContextOptions<EmploymentDbContext> options) : base(options) { }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+
+            modelBuilder.Entity<ApplicantVacancy>()
+            .HasKey(av => new { av.UserId, av.VacancyId });
+
+            modelBuilder.Entity<ApplicantVacancy>()
+                .HasOne(av => av.User)
+                .WithMany(u => u.ApplicantVacancies)
+                .HasForeignKey(av => av.UserId);
+
+            modelBuilder.Entity<ApplicantVacancy>()
+                .HasOne(av => av.Vacancy)
+                .WithMany(v => v.ApplicantVacancies)
+                .HasForeignKey(av => av.VacancyId);
+
+
+            modelBuilder.Entity<User>()
+                .HasOne(u => u.Role)
+                .WithMany(r => r.Users)
+                .HasForeignKey(u => u.RoleId);
+
             modelBuilder.Entity<Vacancy>()
             .HasOne(v => v.Employer)
             .WithMany(u => u.Vacancies)
@@ -24,7 +44,7 @@ namespace PaySky.Infrastructure
             modelBuilder.Entity<Role>()
                 .HasData(
                    new Role { Id=1,Name="Employer"},
-                   new Role { Id = 1, Name = "Applicant" }
+                   new Role { Id = 2, Name = "Applicant" }
             );
 
         }
---
+++ PaySky.Infrastructure/Migrations/20250215142306_MigV001.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace PaySky.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigV001 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            
+
+           
+
+           
+
+           
+
+            migrationBuilder.InsertData(
+                table: "Roles",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Employer" },
+                    { 2, "Applicant" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Applicant_Vacancy_VacancyId",
+                table: "Applicant_Vacancy",
+                column: "VacancyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_RoleId",
+                table: "Users",
+                column: "RoleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Vacancies_EmployerId",
+                table: "Vacancies",
+                column: "EmployerId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Applicant_Vacancy");
+
+            migrationBuilder.DropTable(
+                name: "Vacancies");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Roles");
+        }
+    }
+}
